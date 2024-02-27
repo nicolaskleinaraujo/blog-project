e84a1ff2f95899073fec2fc643cc7d447a4d0fb2
nicolaskleinaraujo <141509949+nicolaskleinaraujo@users.noreply.github.com>
2024-02-27 09:10:05 -0300

    added categories controller to the index file

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,8 @@ const app = express()
 const bodyParser = require("body-parser")
 const connection = require("./database/db")
 
+const categoriesController = require("./categories/CategoriesController")
+
 // Body Parser
 app.use(
   bodyParser.urlencoded({
@@ -12,9 +14,13 @@ app.use(
 app.use(express.json())
 
 // Database
-connection.authenticate()
-.then(() => console.log("Connected to the database"))
-.catch((err) => console.log(err))
+connection
+  .authenticate()
+  .then(() => console.log("Connected to the database"))
+  .catch((err) => console.log(err))
+
+//
+app.use("/", categoriesController)
 
 app.get("/", (req, res) => {
   res.send("Bem-vindo ao meu site")
