81794d0425628799ef0a5e830749928ca69f3dfe
nicolaskleinaraujo <141509949+nicolaskleinaraujo@users.noreply.github.com>
2024-02-27 09:05:59 -0300

    added the connection to the index.js

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,20 @@
 const express = require("express")
 const app = express()
 const bodyParser = require("body-parser")
+const connection = require("./database/db")
 
+// Body Parser
 app.use(
   bodyParser.urlencoded({
     extended: false,
   })
 )
 app.use(express.json())
+
+// Database
+connection.authenticate()
+.then(() => console.log("Connected to the database"))
+.catch((err) => console.log(err))
 
 app.get("/", (req, res) => {
   res.send("Bem-vindo ao meu site")
