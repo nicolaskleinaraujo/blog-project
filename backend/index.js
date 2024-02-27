752a114804410fd360a464526157f6750a67e3d0
nicolaskleinaraujo <141509949+nicolaskleinaraujo@users.noreply.github.com>
2024-02-27 15:01:02 -0300

    added cors to the backend server

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,6 @@
 const express = require("express")
 const app = express()
+const cors = require("cors")
 const bodyParser = require("body-parser")
 const connection = require("./database/db")
 
@@ -9,13 +10,14 @@ const articlesController = require("./articles/ArticlesController")
 const Category = require("./categories/Category")
 const Article = require("./articles/Article")
 
-// Body Parser
+// Body Parser and Cors
 app.use(
   bodyParser.urlencoded({
     extended: false,
   })
 )
 app.use(express.json())
+app.use(cors())
 
 // Database
 connection
