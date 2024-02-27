abb1700f813fc8d71d404177a78ca40566566f73
nicolaskleinaraujo <141509949+nicolaskleinaraujo@users.noreply.github.com>
2024-02-27 14:58:50 -0300

    created axios config and category config

--- frontend/src/Pages/Category/Category.jsx
+++ frontend/src/Pages/Category/Category.jsx
@@ -1,12 +1,40 @@
 // CSS
 import styles from "./Category.module.css"
 
+// Modules
+import { useState } from "react"
+import dbFetch from "../../axios/config"
+
 const Category = () => {
-  return (
-    <div>
-        <h1>Category</h1>
-    </div>
-  )
+    const [title, setTitle] = useState("")
+
+    const handleSubmit = async(e) => {
+        e.preventDefault()
+        try {
+            await dbFetch.post("/categories/save", { title })
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+    return (
+        <div>
+            <h1>Criar nova categoria</h1>
+            <form onSubmit={handleSubmit}>
+                <label>
+                    <p>Titulo: </p>
+                    <input 
+                        type="text" 
+                        name="title" 
+                        id="title"
+                        onChange={(e) => setTitle(e.target.value)}
+                        value={title}
+                    />
+                </label>
+                <button type="submit">Adicionar</button>
+            </form>
+        </div>
+    )
 }
 
 export default Category
