a216088242f73b4284a8783665825d24a7761940
nicolaskleinaraujo <141509949+nicolaskleinaraujo@users.noreply.github.com>
2024-02-28 20:24:00 -0300

    created form for new article page

--- frontend/src/Pages/NewArticle/NewArticle.jsx
+++ frontend/src/Pages/NewArticle/NewArticle.jsx
@@ -1,11 +1,63 @@
+// CSS
 import styles from "./NewArticle.module.css"
 
+// Modules
+import dbFetch from "../../axios/config"
+import { useState, useEffect } from "react"
+
 const NewArticle = () => {
-  return (
-    <div>
-        <h1>NewArticle</h1>
-    </div>
-  )
+    const [title, setTitle] = useState("")
+    const [body, setBody] = useState("")
+    const [category, setCategory] = useState("")
+
+    const [categories, setCategories] = useState([])
+    const getCategories = async() => {
+        const res = await dbFetch.get("/admin/articles/new")
+        setCategories(res.data)
+    }
+
+    useEffect(() => {
+        getCategories()
+    })
+
+    return (
+        <div>
+            <h1>NewArticle</h1>
+            <button onClick={() => console.log(title, body, categories)}></button>
+            <form>
+                <label>
+                    <p>Titulo: </p>
+                    <input 
+                        type="text" 
+                        name="title" 
+                        id="title" 
+                        onChange={(e) => setTitle(e.target.value)}
+                        value={title}
+                    />
+                </label>
+                <label>
+                    <p>Texto: </p>
+                    <input 
+                        type="text" 
+                        name="body" 
+                        id="body" 
+                        onChange={(e) => setBody(e.target.value)}
+                        value={body}
+                    />
+                </label>
+                <label>
+                    <select>
+                        <option disabled selected>--- SELECIONE UMA CATEGORIA ---</option>
+                        {categories && 
+                            categories.map((category) => (
+                                <option value={category.id}>{category.title}</option>
+                            ))
+                        }
+                    </select>
+                </label>
+            </form>
+        </div>
+    )
 }
 
 export default NewArticle
