09fc2d2262a02b5c2b7f299c3bad89e73259f8a2
nicolaskleinaraujo <141509949+nicolaskleinaraujo@users.noreply.github.com>
2024-02-28 19:55:11 -0300

    added dotenv and timezone config to the database

--- backend/database/db.js
+++ backend/database/db.js
@@ -1,8 +1,10 @@
+require('dotenv').config()
 const Sequelize = require("sequelize")
 
-const connection = new Sequelize("blog_project", "root", "Geleia1126?", {
+const connection = new Sequelize("blog_project", "root", process.env.db_pass, {
   host: "localhost",
   dialect: "mysql",
+  timezone: "-03:00",
 })
 
 module.exports = connection
