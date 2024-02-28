7084036b11e6f6766d125ecdb49938a8a3a3083e
nicolaskleinaraujo <141509949+nicolaskleinaraujo@users.noreply.github.com>
2024-02-28 10:29:27 -0300

    added form to the update category page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,6 +5,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom"
 import Home from "./Pages/Home/Home"
 import AddCategory from "./Pages/AddCategory/AddCategory"
 import Categories from "./Pages/Categories/Categories"
+import UpdateCategory from "./Pages/UpdateCategory/UpdateCategory"
 
 function App() {
   return (
@@ -14,7 +15,7 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/categories" element={<Categories />} />
           <Route path="/add-category" element={<AddCategory />} />
-          <Route path="/update-category" element={<UpdateCategory />} />
+          <Route path="/update-category" element={<UpdateCategory id={30} />} />
         </Routes>
       </BrowserRouter>
     </>
--- frontend/src/Pages/UpdateCategory/UpdateCategory.jsx
+++ frontend/src/Pages/UpdateCategory/UpdateCategory.jsx
@@ -1,11 +1,47 @@
+// CSS
 import styles from "./UpdateCategory.module.css"
 
-const UpdateCategory = () => {
-  return (
-    <div>
-        <h1>Update Category</h1>
-    </div>
-  )
+// Modules
+import dbFetch from "../../axios/config"
+import { useState, useLayoutEffect } from "react"
+
+const UpdateCategory = ({ id }) => {
+    const [category, setCategory] = useState([])
+    const [title, setTitle] = useState("")
+
+    const getCategory = async(id) => {
+        const res = await dbFetch.get(`/categories/${id}`)
+        setCategory(res.data)
+        setTitle(category.title)
+    }
+
+    const handleSubmit = () => {
+        console.log("submited")
+    }
+
+    useLayoutEffect(() => {
+        getCategory()
+    }, [])
+
+    return (
+        <div>
+            <h1>Atualizar Categoria</h1>
+            <button onClick={() => console.log(title)}>teste</button>
+            <form onSubmit={handleSubmit}>
+                <label>
+                    <p>Titulo: </p>
+                    <input 
+                        type="text" 
+                        name="title" 
+                        id="title"
+                        onChange={(e) => setTitle(e.target.value)}
+                        value={title}
+                    />
+                </label>
+                <button type="submit">Adicionar</button>
+            </form>
+        </div>
+    )
 }
 
 export default UpdateCategory
