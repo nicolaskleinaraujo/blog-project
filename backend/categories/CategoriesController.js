4489803adb880ec7af34c44679414a47d82a9ee2
nicolaskleinaraujo <141509949+nicolaskleinaraujo@users.noreply.github.com>
2024-02-27 21:58:59 -0300

    added status code to the categories controller

--- backend/categories/CategoriesController.js
+++ backend/categories/CategoriesController.js
@@ -5,14 +5,17 @@ const slugify = require("slugify")
 
 router.post("/categories/save", (req, res) => {
   const title = req.body.title
-  if (title != undefined) {
-    Category.create({
-      title,
-      slug: slugify(title),
-    })
-  } else {
+
+  if (title === undefined) {
+    res.status(400).json({ message: 'Failed to save category' })
     return
   }
+
+  Category.create({
+    title,
+    slug: slugify(title),
+  })
+  res.status(200).json({ message: "Category added succesfully" })
 })
 
 router.get("/admin/categories", async (req, res) => {
@@ -21,20 +24,24 @@ router.get("/admin/categories", async (req, res) => {
     res.status(200).json(categories)
   } catch (error) {
     console.log(error)
+    res.status(400).json({ message: error })
   }
 })
 
 router.delete("/categories/delete/:id", (req, res) => {
   const id = req.params.id
-  if (!isNaN(id)) {
-    Category.destroy({
-      where: {
-        id: id
-      }
-    })
-  } else {
+
+  if (isNaN(id)) {
+    res.status(400).json({ message: `${id} isn't a number` })
     return
   }
+
+  Category.destroy({
+    where: {
+      id: id
+    }
+  })
+  res.status(200).json({ message: "Category removed succesfully" })
 })
 
 module.exports = router
