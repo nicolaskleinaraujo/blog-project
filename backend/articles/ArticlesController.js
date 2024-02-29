a7a8fc9d9ddf1599d2f8b64cf5b24c9b42f76238
nicolaskleinaraujo <141509949+nicolaskleinaraujo@users.noreply.github.com>
2024-02-29 08:47:40 -0300

    added validation to the add category route

--- backend/articles/ArticlesController.js
+++ backend/articles/ArticlesController.js
@@ -20,12 +20,18 @@ router.post("/articles/save", (req, res) => {
   const body = req.body.body
   const category = req.body.category
 
+  if (title === undefined || body === undefined || category === undefined){
+    res.status(400).json({ message: "Failed to save article" })
+    return
+  }
+
   Article.create({
     title,
     slug: slugigy(title),
     body,
     categoryId: category,
   })
+  res.status(200).json({ message: "Article created succesfully" })
 })
 
 module.exports = router
