8933bf3fc60c83b7a482a21c9e00728ec97b6c66
nicolaskleinaraujo <141509949+nicolaskleinaraujo@users.noreply.github.com>
2024-02-29 09:57:09 -0300

    added articles to the home page

--- frontend/src/Pages/Home/Home.jsx
+++ frontend/src/Pages/Home/Home.jsx
@@ -1,10 +1,35 @@
 // CSS
 import styles from "./Home.module.css"
 
+// Modules
+import dbFetch from "../../axios/config"
+import { useState, useEffect } from "react"
+import { Link } from "react-router-dom"
+
 const Home = () => {
+  const [articles, setArticles] = useState([])
+  const getArticles = async() => {
+    const res = await dbFetch.get("/admin/articles")
+    setArticles(res.data)
+  }
+
+  useEffect(() => {
+    getArticles()
+  })
+
   return (
     <div>
         <h1>Home</h1>
+        {articles &&
+          articles.map((article) => (
+            <div key={article.id}>
+              <hr />
+              {article.title} <br />
+              <Link>LER MAIS</Link>
+              <hr />
+            </div>
+          ))
+        }
     </div>
   )
 }
