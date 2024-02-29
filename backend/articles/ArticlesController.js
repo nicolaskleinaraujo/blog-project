ace236b6d06573bcfa43466dee2d7479d9a6a39f
nicolaskleinaraujo <141509949+nicolaskleinaraujo@users.noreply.github.com>
2024-02-29 08:50:36 -0300

    removed get categories from articles controller

--- backend/articles/ArticlesController.js
+++ backend/articles/ArticlesController.js
@@ -1,18 +1,7 @@
 const express = require("express")
 const router = express.Router()
-const Category = require("../categories/Category")
 const Article = require("./Article")
 const slugigy = require("slugify")
-
-// Get All Categories
-router.get("/admin/articles/new", async (req, res) => {
-  try {
-    const categories = await Category.findAll()
-    res.status(200).json(categories)
-  } catch (error) {
-    res.status(400).json({ message: error })
-  }
-})
 
 // Article Save Route
 router.post("/articles/save", (req, res) => {
