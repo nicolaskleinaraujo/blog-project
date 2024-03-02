14175744c27c5aeed96d942a5bda652e2adf822d
nicolaskleinaraujo <141509949+nicolaskleinaraujo@users.noreply.github.com>
2024-03-02 13:06:29 -0300

    added read article

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,6 +8,7 @@ import Categories from "./Pages/Categories/Categories"
 import UpdateCategory from "./Pages/UpdateCategory/UpdateCategory"
 import NewArticle from "./Pages/NewArticle/NewArticle"
 import Articles from "./Pages/Articles/Articles"
+import ReadArticle from "./Pages/ReadArticle/ReadArticle"
 
 function App() {
   return (
@@ -20,6 +21,7 @@ function App() {
           <Route path="/update-category/:id" element={<UpdateCategory />} />
           <Route path="/articles" element={<Articles />} />
           <Route path="new-article" element={<NewArticle />} />
+          <Route path="read-article/:slug" element={<ReadArticle />} />
         </Routes>
       </BrowserRouter>
     </>
---
+++ frontend/src/Pages/ReadArticle/ReadArticle.jsx
@@ -0,0 +1,30 @@
+// CSS
+import styles from "./ReadArticle.module.css"
+
+// Modules
+import dbFetch from "../../axios/config"
+import { useEffect, useState } from "react"
+import { useParams } from "react-router-dom"
+
+const ReadArticle = () => {
+    const { slug } = useParams()
+
+    const [article, setArticle] = useState([])
+    const getArticle = async() => {
+        const res = await dbFetch.get(`/article/${slug}`)
+        setArticle(res.data)
+    }
+
+    useEffect(() => {
+        getArticle()
+    })
+
+    return (
+        <div>
+            <h1>{article.title}</h1>
+            <p>{article.body}</p>
+        </div>
+    )
+}
+
+export default ReadArticle
