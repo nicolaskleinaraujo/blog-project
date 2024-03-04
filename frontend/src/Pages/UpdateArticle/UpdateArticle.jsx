f3f886904ad6a526f903cc4ccbce6defc692f404
nicolaskleinaraujo <141509949+nicolaskleinaraujo@users.noreply.github.com>
2024-03-04 14:12:15 -0300

    removed everything from update article page

--- frontend/src/Pages/UpdateArticle/UpdateArticle.jsx
+++ frontend/src/Pages/UpdateArticle/UpdateArticle.jsx
@@ -1,11 +1,17 @@
+// CSS
 import styles from "./UpdateArticle.module.css"
 
+// Modules
+import dbFetch from "../../axios/config"
+import { useEffect, useState } from "react"
+import { useParams } from "react-router-dom"
+
 const UpdateArticle = () => {
-  return (
-    <div>
-        <h1>UPDATE ARTICLE</h1>
-    </div>
-  )
+    return (
+        <div>
+            <h1>Atualizar Artigo</h1>
+        </div>
+    )
 }
 
 export default UpdateArticle
