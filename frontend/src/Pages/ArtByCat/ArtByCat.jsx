1762f51c9c88514f2106f3f46d542a266e8b5f60
nicolaskleinaraujo <141509949+nicolaskleinaraujo@users.noreply.github.com>
2024-03-04 12:34:07 -0300

    add articles to the get articles by category page

--- frontend/src/Pages/ArtByCat/ArtByCat.jsx
+++ frontend/src/Pages/ArtByCat/ArtByCat.jsx
@@ -1,12 +1,41 @@
 // CSS
 import styles from "./ArtByCat.module.css"
 
+// Modules
+import dbFetch from "../../axios/config"
+import { useState, useEffect } from "react"
+import { useParams, Link } from "react-router-dom"
+
 const ArtByCat = () => {
-  return (
-    <div>
-        <h1>Articles By Category</h1>
-    </div>
-  )
+    const { slug } = useParams()
+
+    const [category, setCategory] = useState([])
+    const [articles, setArticles] = useState([])
+    const getArticles = async() => {
+        const res = await dbFetch.get(`/category/${slug}`)
+        setCategory(res.data)
+        setArticles(res.data.articles)
+    }
+
+    useEffect(() => {
+        getArticles()
+    }, [])
+
+    return (
+        <div>
+            <h1>{category.title}</h1>
+            {articles &&
+                articles.map((article) => (
+                    <div key={article.id}>
+                        <hr />
+                        {article.title} <br />
+                        <Link to={`/${article.slug}`}>LER MAIS</Link>
+                        <hr />
+                    </div>
+                ))
+            }
+        </div>
+    )
 }
 
 export default ArtByCat
