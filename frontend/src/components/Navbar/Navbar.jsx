768318a8af2ca12867e9d0086fb934f3da6ebe34
nicolaskleinaraujo <141509949+nicolaskleinaraujo@users.noreply.github.com>
2024-03-04 12:00:13 -0300

    created navbar component

---
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,27 @@
+// CSS
+import styles from "./Navbar.module.css"
+
+// Modules
+import dbFetch from "../../axios/config"
+import { useEffect, useState } from "react"
+import { Link } from "react-router-dom"
+
+const Navbar = () => {
+    const [categories, setCategories] = useState()
+    const getCategories = async() => {
+        const res = await dbFetch.get("/admin/categories")
+        setCategories(res.data)
+    }
+
+    useEffect(() => {
+        getCategories()
+    }, [])
+
+    return (
+        <nav>
+
+        </nav>
+    )
+}
+
+export default Navbar
