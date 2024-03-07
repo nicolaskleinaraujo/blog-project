6844d2d839ff754a464b6d1386722940ab7da9b2
nicolaskleinaraujo <141509949+nicolaskleinaraujo@users.noreply.github.com>
2024-03-07 13:52:41 -0300

    created authenticate login route

--- backend/users/UsersController.js
+++ backend/users/UsersController.js
@@ -3,10 +3,6 @@ const router = express.Router()
 const User = require("./User")
 const bcrypt = require("bcryptjs")
 
-router.get("/admin/users", (req, res) => {
-  res.send("rota de listagem de usuarios")
-})
-
 // User save route
 router.post("/users/save", async (req, res) => {
   const email = req.body.email
@@ -15,7 +11,7 @@ router.post("/users/save", async (req, res) => {
   const testEmail = await User.findOne({
     where: { email },
   })
-  if (testEmail != undefined){
+  if (testEmail != undefined) {
     res.status(400).json({ message: "This email is already cadastered" })
     return
   }
@@ -33,6 +29,36 @@ router.post("/users/save", async (req, res) => {
     password: hash,
   })
   res.status(200).json({ message: "User created succesfully" })
+})
+
+// Authenticate login route
+router.post("/authenticate", async (req, res) => {
+  const email = req.body.email
+  const password = req.body.password
+
+  const user = await User.findOne({
+    where: {
+      email,
+    },
+  })
+  if (user === undefined) {
+    res.status(400).json({ message: "Incorrect login" })
+    return
+  }
+
+  try {
+    const testPassword = bcrypt.compareSync(password, user.password)
+
+    if (testPassword) {
+      req.session.user = {
+        id: user.id,
+        email: user.email,
+      }
+    }
+    res.status(200).json({ message: "Logged succesfully" })
+  } catch (err) {
+    res.status(400).json(err)
+  }
 })
 
 module.exports = router
