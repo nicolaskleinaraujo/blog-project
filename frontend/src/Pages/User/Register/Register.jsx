af6bf6330ade735b1030715c2ad57de72a50e6a9
nicolaskleinaraujo <141509949+nicolaskleinaraujo@users.noreply.github.com>
2024-03-07 14:29:05 -0300

    created register form

--- frontend/src/Pages/User/Register/Register.jsx
+++ frontend/src/Pages/User/Register/Register.jsx
@@ -1,12 +1,50 @@
 // CSS
 import styles from "./Register.module.css"
 
+// Modules
+import dbFetch from "../../../axios/config"
+import { useState } from "react"
+import { useNavigate } from "react-router-dom"
+
 const Register = () => {
-  return (
-    <div>
-        <h1>Criar Conta</h1>
-    </div>
-  )
+    const navigate = useNavigate()
+
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+
+    const handleSubmit = async(e) => {
+        e.preventDefault()
+    }
+
+    return (
+        <div>
+            <h1>Criar Conta</h1>
+
+            <form onSubmit={handleSubmit}>
+                <label>
+                    <span>Email: </span>
+                    <input 
+                        type="email" 
+                        name="email" 
+                        id="email" 
+                        onChange={(e) => setEmail(e.target.value)} 
+                        value={email} 
+                    />
+                </label>
+
+                <label>
+                    <span>Senha: </span>
+                    <input 
+                        type="password" 
+                        name="password" 
+                        id="password" 
+                        onChange={(e) => setPassword(e.target.value)} 
+                        value={password} 
+                    />
+                </label>
+            </form>
+        </div>
+    )
 }
 
 export default Register
