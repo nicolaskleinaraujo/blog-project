a8606d76909d14a88798f98a2793d61416b39dea
nicolaskleinaraujo <141509949+nicolaskleinaraujo@users.noreply.github.com>
2024-03-07 10:59:59 -0300

    created user save route

--- backend/users/UsersController.js
+++ backend/users/UsersController.js
@@ -3,7 +3,24 @@ const router = express.Router()
 const User = require("./User")
 
 router.get("/admin/users", (req, res) => {
-    res.send("rota de listagem de usuarios")
+  res.send("rota de listagem de usuarios")
+})
+
+// User save route
+router.post("/users/save", async (req, res) => {
+  const email = req.body.email
+  const password = req.body.password
+
+  if (email === undefined || password === undefined) {
+    res.status(400).json({ message: "Failed to save user" })
+    return
+  }
+
+  await User.create({
+    email,
+    password,
+  })
+  res.status(200).json({ message: "User created succesfully" })
 })
 
 module.exports = router
