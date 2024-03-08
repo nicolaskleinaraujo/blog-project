a72d77796960379c950ee8c9cc02a8950d3cd43e
nicolaskleinaraujo <141509949+nicolaskleinaraujo@users.noreply.github.com>
2024-03-07 21:27:22 -0300

    hotfix "1+1" log

--- backend/database/db.js
+++ backend/database/db.js
@@ -1,9 +1,10 @@
-require('dotenv').config()
+require("dotenv").config()
 const Sequelize = require("sequelize")
 
 const connection = new Sequelize("blog_project", "root", process.env.db_pass, {
   host: "localhost",
   dialect: "mysql",
+  logging: false,
   timezone: "-03:00",
 })
 
