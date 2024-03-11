c338e37e65a25d10243552fd8a20812c6d241a37
nicolaskleinaraujo <141509949+nicolaskleinaraujo@users.noreply.github.com>
2024-03-11 14:12:31 -0300

    added localStorage validation on navbar component

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -3,11 +3,8 @@ import styles from "./Navbar.module.css"
 
 // Modules
 import dbFetch from "../../axios/config"
-import { useEffect, useState, useContext } from "react"
+import { useEffect, useState } from "react"
 import { Link } from "react-router-dom"
-
-// Context
-import { AuthContext } from "../../context/AuthContext"
 
 const Navbar = () => {
     const [categories, setCategories] = useState()
@@ -16,11 +13,10 @@ const Navbar = () => {
         setCategories(res.data)
     }
 
-    const { auth, setAuth } = useContext(AuthContext)
     const logOut = async() => {
         if(confirm("Deslogar do sistema?")) {
             localStorage.removeItem("token")
-            setAuth(false)
+            localStorage.setItem("authenticated", false)
         }
     }
 
@@ -38,7 +34,7 @@ const Navbar = () => {
                     ))
                 }
 
-                {auth ? (
+                {localStorage.getItem("authenticated") === "true" ? (
                         <li><button onClick={() => logOut()}>SAIR</button></li>
                     ) : (
                         <Link to="/login">LOGAR</Link>
