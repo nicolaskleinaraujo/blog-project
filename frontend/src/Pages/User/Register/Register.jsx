f6f58d0b9728510b06d7a11e853be8b474ca3c43
nicolaskleinaraujo <141509949+nicolaskleinaraujo@users.noreply.github.com>
2024-03-13 20:06:01 -0300

    added css to the register page

--- frontend/src/Pages/User/Register/Register.jsx
+++ frontend/src/Pages/User/Register/Register.jsx
@@ -27,27 +27,29 @@ const Register = () => {
     }
 
     return (
-        <div>
+        <div className={styles.register}>
             <h1>Criar Conta</h1>
 
             <form onSubmit={handleSubmit}>
+                <p>Crie sua conta para compartilhar seus conhecimentos</p>
+
                 <label>
-                    <span>Email: </span>
                     <input 
                         type="email" 
                         name="email" 
                         id="email" 
+                        placeholder="Digite um email" 
                         onChange={(e) => setEmail(e.target.value)} 
                         value={email} 
                     />
                 </label>
 
                 <label>
-                    <span>Senha: </span>
                     <input 
                         type="password" 
                         name="password" 
                         id="password" 
+                        placeholder="Digite uma senha" 
                         onChange={(e) => setPassword(e.target.value)} 
                         value={password} 
                     />
