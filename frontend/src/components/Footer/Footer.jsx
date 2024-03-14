0585ecb4acca8b4c7960456f5cc171bcf0544ad2
nicolaskleinaraujo <141509949+nicolaskleinaraujo@users.noreply.github.com>
2024-03-14 14:03:43 -0300

    added target blank on footer link

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -4,7 +4,7 @@ import styles from "./Footer.module.css"
 const Footer = () => {
   return (
     <footer className={styles.footer}>
-        <p>&copy; 2024 - Site desenvolvido por <a href="https://github.com/nicolaskleinaraujo">Nicolas Klein</a>.</p>
+        <p>&copy; 2024 - Site desenvolvido por <a href="https://github.com/nicolaskleinaraujo" target="_blank">Nicolas Klein</a></p>
         <p>Favor, utilizar as features com moderação e consciencia.</p>
     </footer>
   )
