5e67246ceb9db907a44f945a2b5500117c6846a3
nicolaskleinaraujo <141509949+nicolaskleinaraujo@users.noreply.github.com>
2024-03-20 10:44:34 -0300

    finalized vps connection to the database

--- backend/database/db.js
+++ backend/database/db.js
@@ -1,8 +1,8 @@
 require("dotenv").config()
 const Sequelize = require("sequelize")
 
-const connection = new Sequelize("blog_project", "root", process.env.db_pass, {
-  host: "localhost",
+const connection = new Sequelize("blog_project", process.env.DB_USERNAME, process.env.DB_PASS, {
+  host: process.env.DB_ADDRESS,
   dialect: "mysql",
   logging: false,
   timezone: "-03:00",
