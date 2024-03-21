8a538b3c8c2fd145c28d0d13fb0be1adee77b555
nicolaskleinaraujo <141509949+nicolaskleinaraujo@users.noreply.github.com>
2024-03-21 10:33:04 -0300

    added loading on readArticle page

--- frontend/src/Pages/Article/ReadArticle/ReadArticle.jsx
+++ frontend/src/Pages/Article/ReadArticle/ReadArticle.jsx
@@ -7,12 +7,14 @@ import { useEffect, useState } from "react"
 import { useParams } from "react-router-dom"
 
 const ReadArticle = () => {
+    const [loading, setLoading] = useState(true)
     const { slug } = useParams()
 
     const [article, setArticle] = useState([])
     const getArticle = async() => {
         const res = await dbFetch.get(`/article/${slug}`)
         setArticle(res.data)
+        setLoading(false)
     }
 
     useEffect(() => {
@@ -21,8 +23,14 @@ const ReadArticle = () => {
 
     return (
         <div className={styles.read_article}>
-            <h1>{article.title}</h1>
-            <p>{article.body}</p>
+            {loading ? (
+                <img src=".././loading.svg" alt="Loading" />
+            ): (
+                <div>
+                    <h1>{article.title}</h1>
+                    <p>{article.body}</p>
+                </div>
+            )}
         </div>
     )
 }
