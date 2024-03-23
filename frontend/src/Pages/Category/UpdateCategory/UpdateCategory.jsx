c0ed651f526fb75b6f511f572e021657441091b5
nicolaskleinaraujo <141509949+nicolaskleinaraujo@users.noreply.github.com>
2024-03-23 14:09:40 -0300

    added loading instace on update category page

--- frontend/src/Pages/Category/UpdateCategory/UpdateCategory.jsx
+++ frontend/src/Pages/Category/UpdateCategory/UpdateCategory.jsx
@@ -7,6 +7,7 @@ import { useState, useEffect } from "react"
 import { useParams, useNavigate } from "react-router-dom"
 
 const UpdateCategory = () => {
+    const [loading, setLoading] = useState(true)
     const navigate = useNavigate()
 
     const { id } = useParams()
@@ -19,19 +20,24 @@ const UpdateCategory = () => {
             }
         })
         setTitle(res.data.category.title)
+        setLoading(false)
     }
 
     const handleSubmit = async(e) => {
         e.preventDefault()
-        try {
-            await dbFetch.post(`/categories/update/${id}`, { title }, {
-                headers: {
-                    'Authorization': `Bearer ${localStorage.getItem("token")}`
-                }
-            })
-            navigate("/categories")
-        } catch (error) {
-            console.log(error)
+
+        if(title != "") {
+            setLoading(true)
+            try {
+                await dbFetch.post(`/categories/update/${id}`, { title }, {
+                    headers: {
+                        'Authorization': `Bearer ${localStorage.getItem("token")}`
+                    }
+                })
+                navigate("/categories")
+            } catch (err) {
+                setLoading(false)
+            }
         }
     }
 
@@ -44,17 +50,24 @@ const UpdateCategory = () => {
             <form onSubmit={handleSubmit}>
                 <h1>Atualizar Categoria</h1>
                 
-                <label>
-                    <input 
-                        type="text" 
-                        name="title" 
-                        id="title" 
-                        placeholder="Digite o titulo" 
-                        value={title} 
-                        onChange={(e) => setTitle(e.target.value)}
-                    />
-                </label>
-                <button type="submit">Atualizar</button>
+                {loading ? (
+                    <img src=".././loading.svg" alt="Loading" />
+                ) : (
+                    <div>
+                        <label>
+                            <input 
+                                type="text" 
+                                name="title" 
+                                id="title" 
+                                placeholder="Digite o titulo" 
+                                value={title} 
+                                onChange={(e) => setTitle(e.target.value)}
+                            />
+                        </label>
+
+                        <button type="submit">Atualizar</button>
+                    </div>
+                )}
             </form>
         </div>
     )
